58af47dfc373cd50bb9f625c351f36d8dbcc3a5d
VijayBhaskar601 <vijaybhaskar601@gmail.com>
2025-10-28 19:34:31 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+From ubuntu
+Run apt update -y && 
+    apt install git -y
+
+
---
+++ Jenkins.sh
@@ -0,0 +1,11 @@
+sudo yum update –y
+yum install java-21-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo yum install git -y
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
